e594422a388a9c88013904c35e5ee368b9dcdc2f
ExQDev <adminator@exqdev.org>
2019-12-23 01:42:03 +0200

    roll back port setrtings

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ async function start () {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host } = nuxt.options.server
-  const port = process.env.PORT || nuxt.options.server.port
+  const { host, port } = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
